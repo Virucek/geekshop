1f143eaff9bad725cf21c09fd1d3a499ff5fea09
Virucek <aykin.yakov@gmail.com>
2020-08-03 02:46:09 +0300

    update geekshop project for lesson 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,7 +12,7 @@ def main(request):
     }
     return render(request, 'mainapp/index.html', context=content)
 
-def catalog(request):
+def catalog(request, pk=None):
     """
     catalog_list = [
         {
@@ -36,19 +36,33 @@ def catalog(request):
 
     ]
     """
-    with open(os.path.join(settings.BASE_DIR, "catalog_list.json"), "r", encoding="utf8") as file:
-        json_data = json.load(file)
+#    with open(os.path.join(settings.BASE_DIR, "catalog_list.json"), "r", encoding="utf8") as file:
+#        json_data = json.load(file)
 
-    catalog_list = json_data
-    for i in catalog_list:
-        i.update({"href": "detail"})
+#    catalog_list = json_data
+#    for i in catalog_list:
+#        i.update({"href": "detail"})
 
+    # Для присваивания определенному подменю класса "active"
+    pk_req = None
+    if pk:
+        pk_req = int([i for i in str(request.path).split('/')][-1])
+
+    if pk == 1 or pk == None:
+        catalog_list = Product.objects.all()
+        title = 'каталог товаров'
+    else:
+        catalog_list = Product.objects.filter(category=pk)
+        title = catalog_list[0].category.name
     submenu_list = ProductCategory.objects.all()
 
+    test = request.path
+
     content = {
-        'title': 'каталог товаров',
+        'title': title,
         'catalog_list': catalog_list,
         'submenu_list': submenu_list,
+        'pk_req': pk_req,
     }
     return render(request, 'mainapp/catalog.html', context=content)
 
